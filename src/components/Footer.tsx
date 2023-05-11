e817da660b8b379dd1b44817ef8bdc9bbe573301
Sebastian Vargas R <s.varggas@gmail.com>
2023-05-11 10:25:34 -0600

    New Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,8 @@
 import { FC } from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Offcanvas from 'react-bootstrap/Offcanvas';
 import { styled } from 'styled-components';
+import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
+
+import Icon from './Icon';
 
 const NavbarContainer = styled(Container)`
     max-width: 60%;
@@ -11,28 +10,46 @@ const NavbarContainer = styled(Container)`
 `;
 
 const Footer:FC = () => {
-  return (
-    <Navbar bg="light">
-        <NavbarContainer fluid>
-            <Navbar.Brand> Sebastian Vargas R. </Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Offcanvas placement="end">
-                <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>
-                        Offcanvas
-                    </Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body>
-                    <Nav className="justify-content-end flex-grow-1 pe-3">
-                        <Nav.Link href="#action1">Profile</Nav.Link>
-                        <Nav.Link href="#action2">Projects</Nav.Link>
-                        <Nav.Link href="#action2">Contact Me</Nav.Link>
-                    </Nav>
-                </Offcanvas.Body>
-            </Navbar.Offcanvas>
-        </NavbarContainer>
-    </Navbar>
-  )
+
+    const icons = [
+        { 
+            name: "LinkedIn", 
+            icon: "bi-linkedin", 
+            href: "https://www.linkedin.com/in/svarggas/" 
+        },
+        { 
+            name: "GitHub", 
+            icon: "bi-github", 
+            href: "https://github.com/svarggas" 
+        },
+        { 
+            name: "CV", 
+            icon: "bi-file-earmark-person", 
+            href: "#",
+        },
+    ];
+
+    return (
+        <Navbar bg="light">
+            <NavbarContainer fluid>
+                <Navbar.Brand>
+                    {
+                        icons.map((icon, index) => (
+                            <Icon key={index}  {...icon} />
+                        ))
+                    }    
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Offcanvas placement="end">
+                    <Offcanvas.Body>
+                        <Nav className="justify-content-end flex-grow-1 pe-3">
+                            <Navbar.Brand> { "<" } Sebastian Vargas R. { " />" } </Navbar.Brand>
+                        </Nav>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </NavbarContainer>
+        </Navbar>
+    );
 }
 
 export default Footer;
